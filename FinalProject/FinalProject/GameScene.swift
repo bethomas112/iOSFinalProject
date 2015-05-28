329b5f3887c0879e86392abf7c42e8f48e4e2bed
Erik Owen <erikjowen@yahoo.com>
2015-05-28 12:09:32 -0700

    changed space ship to car

--- FinalProject/FinalProject/GameScene.swift
+++ FinalProject/FinalProject/GameScene.swift
@@ -34,7 +34,8 @@ class GameScene: SKScene {
     
     func initializeScene() {
         initBackground()
-        initAccelerometer()
+        initCar()
+        initButtons()
     }
     
     func initBackground() {
@@ -68,33 +69,13 @@ class GameScene: SKScene {
         }
     }
         
-    func initAccelerometer() {
-        self.size = self.view!.frame.size
-        let demo = SKLabelNode(fontNamed:"AppleSDGothicNeo-Regular")
-        demo.text = "Accelerometer Demo";
-        demo.fontSize = 30;
-        demo.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame) + 225)
-        self.addChild(demo)
-        
-        let button = SKShapeNode(rectOfSize: CGSize(width: 150.0, height: 40.0))
-        button.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame) + 150)
-        button.fillColor = UIColor.grayColor()
-        button.name = "swapSceneButton"
-        
-        let buttonLabel = SKLabelNode(fontNamed: "AppleSDGothicNeo-Regular")
-        buttonLabel.text = "ParseDemo"
-        buttonLabel.fontSize = 20;
-        buttonLabel.color = UIColor.greenColor()
-        //buttonLabel.position = button.position
-        button.addChild(buttonLabel)
-        self.addChild(button)
-        
-        ship = SKSpriteNode(imageNamed:"Spaceship")
+    func initCar() {
+        ship = SKSpriteNode(imageNamed:"player_car")
         motionManager = CMMotionManager()
         ship.xScale = 0.3
         ship.yScale = 0.3
-        ship.zPosition = 2
-        ship.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2 - 250)
+        ship.zPosition = 4
+        ship.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2 - 275)
         self.addChild(ship)
         // motionManager.accelerometerUpdateInterval = NSTimeInterval(0.05)
         
@@ -121,6 +102,30 @@ class GameScene: SKScene {
             })
         }
 
+    }
+    
+    func initButtons() {
+        self.size = self.view!.frame.size
+        let demo = SKLabelNode(fontNamed:"AppleSDGothicNeo-Regular")
+        demo.text = "Accelerometer Demo";
+        demo.fontSize = 30;
+        demo.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame) + 225)
+        demo.zPosition = 3
+        self.addChild(demo)
+        
+        let button = SKShapeNode(rectOfSize: CGSize(width: 150.0, height: 40.0))
+        button.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame) + 150)
+        button.fillColor = UIColor.grayColor()
+        button.name = "swapSceneButton"
+        button.zPosition = 3
+        
+        let buttonLabel = SKLabelNode(fontNamed: "AppleSDGothicNeo-Regular")
+        buttonLabel.text = "ParseDemo"
+        buttonLabel.fontSize = 20;
+        buttonLabel.color = UIColor.greenColor()
+        buttonLabel.zPosition - 3
+        button.addChild(buttonLabel)
+        self.addChild(button)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
