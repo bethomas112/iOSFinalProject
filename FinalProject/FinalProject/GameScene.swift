15d9f38f12f37dbb1ffb06bf833c55ae2b09293e
Erik Owen <erikjowen@yahoo.com>
2015-05-28 11:51:44 -0700

    added scrolling background

--- FinalProject/FinalProject/GameScene.swift
+++ FinalProject/FinalProject/GameScene.swift
@@ -10,11 +10,21 @@ import SpriteKit
 import CoreMotion
 
 class GameScene: SKScene {
+    
+    /*Accerometer Movement*/
     let moveSpeed: CGFloat = 500.0
     var ship = SKSpriteNode()
     var motionManager = CMMotionManager()
     var destX:CGFloat = 0.0
     var destY:CGFloat = 0.0
+    
+    /*Background*/
+    var background: SKNode!;
+    let background_speed = 100.0;
+    
+    /*Time Values*/
+    var delta = NSTimeInterval(0);
+    var last_update_time = NSTimeInterval(0);
     
     var swapScene : (() -> Bool)?
     
@@ -23,7 +33,42 @@ class GameScene: SKScene {
     }
     
     func initializeScene() {
-        /* Setup your scene here */
+        initBackground()
+        initAccelerometer()
+    }
+    
+    func initBackground() {
+        background = SKNode();
+        addChild(background);
+        
+        for i in 0...2 {
+            let tile = SKSpriteNode(imageNamed: "background");
+            tile.anchorPoint = CGPointZero
+            tile.position = CGPoint(x: 0.0, y: CGFloat(i) * 568)
+            tile.name = "bg"
+            tile.zPosition = 1
+            background.addChild(tile)
+        }
+    }
+    
+    func moveBackground() {
+        let posY = -background_speed * delta;
+        background.position = CGPoint(x: 0.0, y: background.position.y + CGFloat(posY));
+        
+        background.enumerateChildNodesWithName("bg") { (node, stop) in
+            let background_screen_position = self.background.convertPoint(node.position, toNode: self);
+            
+            let difference = self.frame.height - node.frame.size.height
+            
+            if background_screen_position.y <= -node.frame.size.height {
+//            if background_screen_position.y <= -node.frame.size.height + difference {
+                node.position = CGPoint(x: node.position.x, y: node.position.y + (node.frame.size.height * 2));
+            }
+            
+        }
+    }
+        
+    func initAccelerometer() {
         self.size = self.view!.frame.size
         let demo = SKLabelNode(fontNamed:"AppleSDGothicNeo-Regular")
         demo.text = "Accelerometer Demo";
@@ -48,6 +93,7 @@ class GameScene: SKScene {
         motionManager = CMMotionManager()
         ship.xScale = 0.3
         ship.yScale = 0.3
+        ship.zPosition = 2
         ship.position = CGPoint(x: frame.size.width/2, y: frame.size.height/2 - 250)
         self.addChild(ship)
         // motionManager.accelerometerUpdateInterval = NSTimeInterval(0.05)
@@ -74,6 +120,7 @@ class GameScene: SKScene {
                 
             })
         }
+
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -94,6 +141,11 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        delta = (last_update_time == 0.0) ? 0.0 : currentTime - last_update_time;
+        last_update_time = currentTime;
+        
+        moveBackground()
+        
         var action = SKAction.moveToX(destX, duration: NSTimeInterval(0.5))
         ship.runAction(action)
     }
