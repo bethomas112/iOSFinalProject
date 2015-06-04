081b75bf7e6f676723477e8d5f98126717ba8dca
Erik Owen <erikjowen@yahoo.com>
2015-06-04 14:30:05 -0700

    started adding obstacles

--- FinalProject/FinalProject/GameScene.swift
+++ FinalProject/FinalProject/GameScene.swift
@@ -33,6 +33,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var swapScene : (() -> Bool)?
     
+    func skRandf() -> CGFloat {
+        return CGFloat(Double(arc4random()) / Double(UINT32_MAX))
+    }
+    
+    func skRand(lowerBound low: CGFloat, upperBound high: CGFloat) -> CGFloat {
+        return skRandf() * (high - low) + low
+    }
+    
     override func didMoveToView(view: SKView) {
         initializeScene()
     }
@@ -42,30 +50,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         initButtons()
         initBackground()
         initWorld()
+        initObstacles()
     }
     
     func initWorld() {
         let wallWidth : CGFloat = self.size.width / 12.0
         
         physicsWorld.contactDelegate = self
-        
-//        let leftWall : SKSpriteNode = SKSpriteNode()
-//        leftWall.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: 0.0, y: 0.0, width: wallWidth, height: self.frame.height))
-//        leftWall.physicsBody?.categoryBitMask = FSBoundaryCategory
-//        leftWall.physicsBody?.collisionBitMask = FSPlayerCategory | FSObstacleCategory
-//        leftWall.physicsBody?.contactTestBitMask = FSPlayerCategory | FSObstacleCategory
-//        leftWall.position = CGPointZero
-//        
-//        self.addChild(leftWall)
-//        
-//        let rightWall : SKSpriteNode = SKSpriteNode()
-//        rightWall.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: self.size.width - wallWidth, y: 0.0, width: wallWidth, height: self.frame.height))
-//        rightWall.physicsBody?.categoryBitMask = FSBoundaryCategory
-//        rightWall.physicsBody?.collisionBitMask = FSPlayerCategory | FSObstacleCategory
-//        rightWall.physicsBody?.contactTestBitMask = FSPlayerCategory | FSObstacleCategory
-//        rightWall.position = CGPointZero
-//        
-//        self.addChild(rightWall)
         
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: wallWidth, y: 0.0, width: self.size.width - (self.size.width / 6.0), height: self.size.height))
         self.physicsBody?.friction = 0.0
@@ -108,7 +99,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         motionManager = CMMotionManager()
         player.xScale = 0.3
         player.yScale = 0.3
-//        player.physicsBody = SKPhysicsBody(rectangleOfSize: player.size)
         player.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: player.size.width * 0.5, height: player.size.height))
         player.physicsBody?.categoryBitMask = FSPlayerCategory
         player.physicsBody?.contactTestBitMask = FSBoundaryCategory | FSObstacleCategory
@@ -116,14 +106,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.physicsBody?.affectedByGravity = false;
         player.physicsBody?.allowsRotation = false;
         player.physicsBody?.restitution = 0.9
-        player.physicsBody?.affectedByGravity = false
         player.physicsBody?.dynamic = true
         player.zPosition = 4
-        player.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 6)
+        player.position = CGPoint(x: 200, y: self.size.height / 6)
         self.destX = player.position.x
         
         self.addChild(player)
-        // motionManager.accelerometerUpdateInterval = NSTimeInterval(0.05)
         
         if motionManager.accelerometerAvailable {
             
@@ -148,6 +136,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             })
         }
 
+    }
+    
+    func initObstacles() {
+        let addObstacle = SKAction.sequence([
+            SKAction.runBlock {
+                self.addObstacle("obstacle1")
+            },
+            SKAction.waitForDuration(2.0, withRange: 0.5)
+            ])
+        
+        let addObstacles = SKAction.repeatActionForever(addObstacle)
+        
+        runAction(addObstacles)
+    }
+    
+    func addObstacle(fileName: String) {
+        let obstacle = SKSpriteNode(imageNamed: fileName)
+        obstacle.position = CGPoint(x: skRand(lowerBound: self.size.width / 12.0, upperBound: self.size.width - (self.size.width / 12.0)), y: self.size.height)
+        obstacle.name = "obstacle"
+        obstacle.zPosition = 4
+        
+        let physicsBody = SKPhysicsBody(circleOfRadius: obstacle.size.width / 2.0)
+        physicsBody.affectedByGravity = false
+        physicsBody.velocity = CGVector(dx: 0, dy: -100)
+        physicsBody.categoryBitMask = FSObstacleCategory
+        physicsBody.contactTestBitMask = FSPlayerCategory | FSBoundaryCategory
+        physicsBody.collisionBitMask = FSPlayerCategory | FSBoundaryCategory
+        obstacle.physicsBody = physicsBody
+        
+        self.addChild(obstacle)
     }
     
     func initButtons() {
